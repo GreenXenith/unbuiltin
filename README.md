e132c38888e91b4b42406bdd9e81dcb293b4955d
GreenXenith <24834740+GreenXenith@users.noreply.github.com>
2024-10-27 23:02:03 -0700

    WIP

---
+++ README.md
@@ -0,0 +1,1 @@
+An attempt to remove as much builtin opinions as possible. WIP.
---
+++ init.lua
@@ -0,0 +1,86 @@
+-- EVERYTHING MUST GO --
+
+-- Reset some registered tables
+for _, registered in ipairs({
+    "abms",
+    "lbms",
+    "entities",
+
+    "playerevents",
+    "privileges",
+
+    "aliases",
+
+    "on_chat_messages",
+    "on_newplayers",
+    "on_dieplayers",
+    "on_respawnplayers",
+    "on_prejoinplayers",
+    "on_joinplayers",
+    "on_leaveplayers",
+    "on_player_receive_fields",
+
+    "chatcommands",
+    "on_chatcommands",
+    "on_punchnodes",
+    "on_placenodes",
+    "on_dignodes",
+}) do
+    local t = core["registered_" .. registered]
+    for i in ipairs(t) do t[i] = nil end
+end
+
+-- Clear the player environment and interface
+minetest.register_on_joinplayer(function(player)
+    player:hud_set_flags({
+        basic_debug = false,
+        chat = false,
+        crosshair = false,
+        hotbar = false,
+        wielditem = false,
+    })
+
+    -- Disabling the minimap without the "disabled" warning
+    player:set_minimap_modes({{type = "off", label = " "}}, 0)
+
+    player:set_properties({
+        visual = "sprite",
+        textures = {"blank.png"},
+        physical = false,
+        eye_height = 0,
+        zoom_fov = 0,
+    })
+
+    player:set_inventory_formspec("")
+    player:set_formspec_prepend("")
+
+    player:set_physics_override({
+        gravity = 0,
+        jump = 0,
+        speed = 0,
+        sneak = false,
+    })
+
+    player:set_sky({
+        type = "plain",
+        base_color = "#888",
+        clouds = false,
+    })
+
+    player:set_sun({
+        visible = false,
+        sunrise_visible = false,
+    })
+
+    player:set_moon({
+        visible = false,
+    })
+
+    player:set_stars({
+        visible = false,
+    })
+
+    player:set_pos(vector.zero())
+end)
+
+minetest.set_timeofday(0.5)
